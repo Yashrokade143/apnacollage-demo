c9e4c2804adda0ff5c1754566205f9a72a091df9
Yashrokade143 <yashsanjaypatilrokade@gmail.com>
2025-12-09 17:12:11 +0530

    addnew int valu

--- yash.java
+++ yash.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class yash{
+    int y = 10;
     public static void main(String... args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number to find factorial:");
@@ -9,10 +10,10 @@ public class yash{
         
     }
     public static void factorial(int num){
-        int fact = 1;
-        for(int i=1;i<=num;i++){
+        int fact = 1;   // add comment in the code 
+        for(int i=1;i<=num;i++){  // fro loop to claculate the factorial 
             fact = fact * i;
         }
-        System.out.println("the factorsial of "+num+" is "+fact);
+        System.out.println("the factorsial of "+num+" is "+fact);  // print heare the organal factorial
     }
 }
